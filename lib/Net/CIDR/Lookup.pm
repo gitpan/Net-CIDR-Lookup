9dd65d6491191fe47445516362719225bd555105
Matthias Bethke <mbethke@cpan.org>
2014-02-06 23:19:25 +0000

    Import of MBETHKE/Net-CIDR-Lookup-0.51_3 from CPAN.
    gitpan-cpan-distribution: Net-CIDR-Lookup
    gitpan-cpan-version:      0.51_3
    gitpan-cpan-path:         MBETHKE/Net-CIDR-Lookup-0.51_3.tar.bz2
    gitpan-cpan-author:       MBETHKE
    gitpan-cpan-maturity:     developer

--- lib/Net/CIDR/Lookup.pm
+++ lib/Net/CIDR/Lookup.pm
@@ -8,13 +8,13 @@ This class implements a lookup table indexed by IPv4 networks or hosts.
 
 =over 1
 
-=item * Addresses are accepted in numeric form (integer with separate netbits argument),
+=item Addresses are accepted in numeric form (integer with separate netbits argument),
 as strings in CIDR notation or as IP address ranges
 
-=item * Overlapping or adjacent networks are automatically coalesced if their
+=item Overlapping or adjacent networks are automatically coalesced if their
 associated values are equal.
 
-=item * The table is implemented as a binary tree so lookup and insertion take O(log n)
+=item The table is implemented as a binary tree so lookup and insertion take O(log n)
 time.
 
 =back
@@ -294,12 +294,12 @@ sub clear {
 
 =over 1
 
-=item *
+=item
 
 I didn't need deletions yet and deleting parts of a CIDR block is a bit more
 complicated than anything this class does so far, so it's not implemented.
 
-=item *
+=item
 
 Storing an C<undef> value does not work and yields an error. This would be
 relatively easy to fix at the cost of some memory so that's more a design
@@ -397,7 +397,22 @@ sub _lookup {
 }
 
 # IPv4 address from dotted-quad to integer
-sub _dq2int { unpack 'N', inet_pton(AF_INET, shift) }
+# Choose manual implementation on Windows where inet_pton() is not available
+if('MSWin32' eq $^O) {
+    sub _dq2int { ## no critic (Subroutines::RequireArgUnpacking)
+        my @oct = split /\./, $_[0];
+        4 == @oct or croak "address must be in dotted-quad form, is `$_[0]'";
+        my $ip = 0;
+        foreach(@oct) {
+            $_ <= 255 and $_ >= 0
+                or croak "invalid component `$_' in address `$_[0]'";
+            $ip = $ip<<8 | $_;
+        }
+        return $ip;
+    }
+} else {
+    sub _dq2int { unpack 'N', inet_pton(AF_INET, shift) }
+}
 
 # IPv4 address from integer to dotted-quad
 sub _int2dq { inet_ntop(AF_INET, pack 'N', shift) }
